9298daa98af419825641b73983bb82d54b1e3578
Tandson42 <tandsonfilho@gmail.com>
2026-04-14 21:45:41 -0300

    Atividade resolvida

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,9 +1,28 @@
+import exercicios.Cidade;
+import exercicios.CidadeRecord;
+import exercicios.PessoaRecord;
+import exercicios.PessoaRecordShallow;
+
 /**
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
 public class Principal {
     public static void main(final String[] args) {
+        final var cr = new CidadeRecord("São Paulo");
+        System.out.println(cr.toString());
 
+        Cidade cidade = new Cidade("Brasilia");
+        cidade.setNome("Palmeiras");
+        System.out.println(cidade.toString());
+
+        final var prs = new PessoaRecordShallow("Tandson",cidade);
+        System.out.println("Antes "+prs.toString());
+        prs.cidade().setNome("Palmas");
+        System.out.println(prs.toString());
+
+        System.out.println("Completamente imutavel");
+        final var pr = new PessoaRecord("Manoel",cr);
+        System.out.println(pr.toString());
     }
 }
